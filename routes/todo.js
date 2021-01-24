496e953a9178e2a054379621194ed220373251b9
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-01-24 12:16:49 +0100

    // prettier-ignore

--- routes/todo.js
+++ routes/todo.js
@@ -5,25 +5,30 @@ const todoCtrl = require('../controllers/todo');
 
 const router = express.Router();
 
+// prettier-ignore
 router.get('/',
   todoCtrl.list,
 );
 
+// prettier-ignore
 router.get('/:id',
   todoCtrl.show,
 );
 
+// prettier-ignore
 router.post('/',
   authenticate,
   express.json(),
   todoCtrl.add
 );
 
+// prettier-ignore
 router.delete('/:id',
   authenticate,
   todoCtrl.delete,
 );
 
+// prettier-ignore
 router.put('/:id',
   authenticate,
   express.json(),
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const userCtrl = require('../controllers/user');
 
 const router = express.Router();
 
+// prettier-ignore
 router.post('/login',
   express.json(),
   userCtrl.login
